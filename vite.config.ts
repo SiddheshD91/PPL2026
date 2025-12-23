d159d23196f8b8f5fef0023552167974b3bd6316
Siddhesh <Siddhesh.Dhole@saint-gobain.com>
2025-12-23 15:55:12 +0530

    updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Cricket-League-Auction/', // set to your repo name for GitHub Pages
+  base: '/PPL2026/',
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
